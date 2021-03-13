bb5b46c956c1ec6665648c5127f0fc7c270d3423
Vaughn Estrada <vmestrada7@gmail.com>
2021-03-13 12:32:54 -0500

    Week 4 Assignment

--- js/script.js
+++ js/script.js
@@ -11,4 +11,20 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+
 });
+
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show');
+     })
+
+});
+
+$(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+     })
+})
+
+
